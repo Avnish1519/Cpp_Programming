a4ce5a758ed7143d597b4ec6968dc62fc0949a1b
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-05-15 17:53:45 +0000

    comments

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -14,7 +14,8 @@ void insertionSort(int arr[], int n) {//function
     }
 }
 
-void printArray(int arr[], int size) {
+void printArray(int arr[], int size) {//function
+    
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }

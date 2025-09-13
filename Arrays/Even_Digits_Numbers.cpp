db4b376dd8b7b6faa1049f8d8a67ebfbd04409ac
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-09-13 10:53:34 +0530

    Update Even_Digits_Numbers.cpp

--- Arrays/Even_Digits_Numbers.cpp
+++ Arrays/Even_Digits_Numbers.cpp
@@ -3,7 +3,7 @@ class Solution {
     public:
         int findNumbers(vector<int>& nums) {
             int ans = 0;
-            for(int val : nums) {
+            for(int val : nums) {//using for loop
                 string s = to_string(val);
                 if(!(s.length() & 1))
                     ans++;

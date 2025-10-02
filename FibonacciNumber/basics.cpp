df54a53f82df8a03094f4dd102c109a76648ec86
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-10-02 20:54:24 +0530

    Update basics.cpp

--- FibonacciNumber/basics.cpp
+++ FibonacciNumber/basics.cpp
@@ -1,7 +1,7 @@
 class Solution {//class
     public:
         int fib(int n) {
-            if(n==0 || n==1){
+            if(n==0 || n==1){//using if-else 
                 return n;
     
             }else{

a21647593c7b589007e3eaefb956da3d93d46421
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-10-07 21:46:48 +0530

    Update basics.cpp

--- FibonacciNumber/basics.cpp
+++ FibonacciNumber/basics.cpp
@@ -1,12 +1,13 @@
-class Solution {//class
-    public:
-        int fib(int n) {
-            if(n==0 || n==1){//using if-else 
-                return n;
-    
-            }else{
-                return fib(n-1) + fib(n-2);
-            }
-            
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0 || n == 1) return n;
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; ++i) {
+            int temp = a + b;
+            a = b;
+            b = temp;
         }
-    };
+        return b;
+    }
+};

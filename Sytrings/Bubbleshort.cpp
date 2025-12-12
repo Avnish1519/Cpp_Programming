0ab26a6cb1e22cf00951a3dda0a62d4cf60e3987
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-12-12 21:53:33 +0530

    comments

--- Sytrings/Bubbleshort.cpp
+++ Sytrings/Bubbleshort.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++) {//using for loop
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);

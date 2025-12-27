200d835d2e8b644094bddf7afb7f8d79e027ca8d
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-12-27 21:35:59 +0530

    comments

--- KadanesAlgorithm/basics.cpp
+++ KadanesAlgorithm/basics.cpp
@@ -18,5 +18,5 @@ int main(){
     int nums[]={1,-3,2,3,-4};//array
     int n=sizeof(nums)/sizeof(nums[0]);
     int maxSubArray=KadanesAlgorithm(nums,n);
-    cout<<maxSubArray<<endl;//printing the output
+    cout<<maxSubArray<<endl;//printing the outputs
 }

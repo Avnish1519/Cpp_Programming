2c8a2b9e0153d19af33f0b20560e82b456ccb438
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-12-30 22:10:14 +0530

    comments

--- FibonacciNumber/basics.cpp
+++ FibonacciNumber/basics.cpp
@@ -1,7 +1,7 @@
 class Solution {//class solution declared
 public:
     int fib(int n) {
-        if (n == 0 || n == 1) return n;
+        if (n == 0 || n == 1) return n;//using if-control statement
         int a = 0, b = 1;
         for (int i = 2; i <= n; ++i) {//using a for-loop
             int temp = a + b;

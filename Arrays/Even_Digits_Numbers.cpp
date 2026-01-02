7a462ebae7fd5b4261f4425edc1ea60063f5c4e7
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2026-01-03 03:56:40 +0530

    comments

--- Arrays/Even_Digits_Numbers.cpp
+++ Arrays/Even_Digits_Numbers.cpp
@@ -2,7 +2,7 @@
 class Solution {
     public:
         int findNumbers(vector<int>& nums) {
-            int ans = 0;
+            int ans = 0;//initializing ans
             for(int val : nums) {//using for loop
                 string s = to_string(val);
                 if(!(s.length() & 1))//using if control statement

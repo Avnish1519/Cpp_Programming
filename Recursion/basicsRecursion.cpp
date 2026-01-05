0d485265fb27c1e600154595d0172fb688361394
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2026-01-05 20:51:40 +0530

    Implement recursive sum function
    Add a recursive function to calculate the sum of integers.

--- Recursion/basicsRecursion.cpp
+++ Recursion/basicsRecursion.cpp
@@ -18,3 +18,19 @@ int main() {
     cout << "Factorial of " << num << " is: " << result << endl;
     return 0;
 }
+-----------------------------------------------------------------------------------------------------------------------------------------------
+#include<iostream>
+using namespace std;
+
+int sum(int  n){
+    if (n == 1)
+     return  1 ;
+    return n + sum(n - 1); 
+}
+
+int main() {
+    int n = 5 ;
+    cout <<  sum(n); 
+    return 0;
+}
+

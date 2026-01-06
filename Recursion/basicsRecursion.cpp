c434a165283fea248362377c3ab5fead77e7da47
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2026-01-06 21:13:41 +0530

    Add factorial function to basicsRecursion.cpp

--- Recursion/basicsRecursion.cpp
+++ Recursion/basicsRecursion.cpp
@@ -33,4 +33,19 @@ int main() {
     cout <<  sum(n); 
     return 0;
 }
+-------------------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+    if (n == 0)
+        return 1;
+    return n * fact(n - 1);
+}
+
+int main()
+{
+    cout << "Factorial of 5 : " << fact(5);
+    return 0;
+}
 

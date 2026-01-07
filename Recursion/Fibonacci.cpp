815ba50574f31fb025178018878fd02bf4a582d0
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2026-01-07 22:36:30 +0530

    Add Fibonacci calculation and sequence generation
    Implement recursive Fibonacci function and generate sequence.

--- Recursion/Fibonacci.cpp
+++ Recursion/Fibonacci.cpp
@@ -20,3 +20,38 @@ int main() {
 
     return 0;
 }
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findFibonacci(int n) {
+  
+   // base case n = 0  n = 1
+   if (n == 0) {
+      return 0;
+   } else if (n == 1) {
+      return 1;
+   } else {
+      return findFibonacci(n - 2) + findFibonacci(n - 1);
+   }
+}
+
+vector<int> fibonacciNumbers(int n) {
+   vector<int> ans;
+   for (int i = 0; i < n; i++) {
+      ans.push_back(findFibonacci(i));
+   }
+   return ans;
+}
+
+int main() {
+   int n = 7;
+   vector<int> res = fibonacciNumbers(n);
+   for (int i = 0; i < res.size(); i++) {
+      cout << res[i] << " ";
+   }
+   return 0;
+}

e3d918cef9237a57e94ee0b605e100e65133941b
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2026-01-21 22:08:03 +0530

    Comments

--- Arrays/SwapAlternate.cpp
+++ Arrays/SwapAlternate.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void printArray(int arr[],int size){
     for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
+        cout<<arr[i]<<" ";//printing the output
     }
     cout<<endl;
 }

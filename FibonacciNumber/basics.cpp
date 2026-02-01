0557d3120140878367d8ccab5075b3b3dfd5aa08
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2026-02-01 23:16:42 +0530

    Fix comment on Fibonacci value swapping
    Corrected comment for clarity on value swapping.

--- FibonacciNumber/basics.cpp
+++ FibonacciNumber/basics.cpp
@@ -4,7 +4,7 @@ public:
         if (n == 0 || n == 1) return n;//using if-control statement
         int a = 0, b = 1;
         for (int i = 2; i <= n; ++i) {//using a for-loop
-            int temp = a + b;
+            int temp = a + b;//swapping values of a with b and vice versa using a temprory variable
             a = b;
             b = temp;
         }
